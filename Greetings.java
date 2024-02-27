095a4fe2cf8c8368196e8cbd89376dd2068ef860
nmk218 <menna.khedr@newcastle.ac.uk>
2024-02-27 14:54:24 +0000

    Second version

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,6 @@
 public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello and greetings to all.");
+        System.out.println("This is a test program");
     }
 }
