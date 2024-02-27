f331c7548cc8aeb261e2af99c4ad6844ec9a85be
nmk218 <menna.khedr@newcastle.ac.uk>
2024-02-27 17:53:34 +0000

    added sout line

--- Greetings.java
+++ Greetings.java
@@ -2,5 +2,6 @@ public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello and greetings to all.");
         System.out.println("This is a test program");
+        System.out.println("Testing terminal");
     }
 }
